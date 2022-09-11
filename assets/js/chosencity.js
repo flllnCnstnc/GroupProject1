0380a56587c8c47862b309ccad525899619a8174
Ari-chan <arielle.elak@gmail.com>
2022-09-11 15:27:57 -0600

    Testing syncronous functions for location and list

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -18,19 +18,46 @@ var restaurantsBlock = $('restaurants-block');
      $(document).prop('title', "Welcome to " + city);
      var restaurantsTitle = $('#restaurants-title').text("Places to Eat in " + city);
 
-     const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '416439bb4fmshfef4ac0068641dap1bc56fjsn3c339a781461',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-        },
-        body: '{"geoId":33388,"sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":12.248278039408776,"longitude":109.1981618106365},"southWestCorner":{"latitude":12.243407232845051,"longitude":109.1921640560031}},"updateToken":""}'
-    };
 
-    fetch('https://travel-advisor.p.rapidapi.com/restaurants/v2/list?currency=USD&units=km&lang=en_US', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+     function getGeoID() {
+         const options = {
+             method: 'GET',
+             headers: {
+                 'X-RapidAPI-Key': '416439bb4fmshfef4ac0068641dap1bc56fjsn3c339a781461',
+                 'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+             }
+         };
+
+         fetch('https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=' + city + '%2C%20co&lang=en_US&units=km', options)
+            .then((response) => response.json())
+             .then((data) => {
+                 console.log(data);
+         })
+         .catch(console.error);
+
+     };
+
+
+     function getrestList() {
+         const options = {
+             method: 'POST',
+             headers: {
+                 'content-type': 'application/json',
+                 'X-RapidAPI-Key': '416439bb4fmshfef4ac0068641dap1bc56fjsn3c339a781461',
+                 'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+             },
+             body: '{"geoId":33388,"partySize":2,"reservationTime":"2022-03-07T20:00","sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"updateToken":""}'
+         };
+
+         fetch('https://travel-advisor.p.rapidapi.com/restaurants/v2/list?currency=USD&units=km&lang=en_US', options)
+             .then((response) => response.json())
+             .then((data) => {
+                 console.log(data);
+             })
+             .catch(console.error);
+     };
+
+     getGeoID();
+     getrestList();
 
  });
