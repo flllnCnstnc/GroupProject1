b6b4225bbda35aa17896c4508c69e00dce29c7a1
Ari-chan <arielle.elak@gmail.com>
2022-09-11 22:44:19 -0600

    Able to traverse down to third level but after that it goes undefined

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -19,6 +19,8 @@ var requestUrl = '';
 $(document).prop('title', "Welcome to " + city);
 var restaurantsTitle = $('#restaurants-title').text("Places to Eat in " + city);
 
+
+
 const options = {
 	method: 'POST',
 	headers: {
@@ -26,10 +28,20 @@ const options = {
 		'X-RapidAPI-Key': '1b5f8f1091mshbc2582aab1d8be2p14f00fjsnce3839947530',
 		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
 	},
-	body: '{"geoId":293928,"partySize":2,"reservationTime":"2022-03-07T20:00","sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":12.248278039408776,"longitude":109.1981618106365},"southWestCorner":{"latitude":12.243407232845051,"longitude":109.1921640560031}},"updateToken":""}'
+	body: '{"geoId":33324,"partySize":2,"reservationTime":"2022-03-07T20:00","sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":12.248278039408776,"longitude":109.1981618106365},"southWestCorner":{"latitude":12.243407232845051,"longitude":109.1921640560031}},"updateToken":""}'
 };
 
 fetch('https://travel-advisor.p.rapidapi.com/restaurants/v2/list?currency=USD&units=km&lang=en_US', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+.then(function (response) {
+    if (response.ok) {
+      console.log(response);
+        response.json().then(function (data) {
+            console.log(data.data.AppPresentation_queryAppListV2);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function (error) {
+    alert('Unable to connect to GitHub');
+  });
