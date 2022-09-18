2ff92d2e78d74828bc2f3354ecb5b50499843701
Ari-chan <arielle.elak@gmail.com>
2022-09-17 20:08:42 -0600

    Testing functionality of search term entry - this will run on page loading

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,18 +5,28 @@
 
 
 var API_KEY = '16784074-a09f6dbadf96547b9a326b052';
-var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent('Boulder Colorado');
 
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function (i, hit) {
-        console.log(hit.pageURL)
 
+// Fetches pictures for each given city search term
+
+var API_KEY = '16784074-a09f6dbadf96547b9a326b052';
+
+
+function fetchImages(searchTerm) {
+    var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchTerm);
+    $.getJSON(URL, function (data) {
+        console.log(URL);
+    if (parseInt(data.totalHits) > 0)
+        $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+    else
+        console.log('No hits');
     });
-else
-    console.log('No hits');
-});
+};
 
-$(".cityBlock").on("click", function () {
-    console.log((this.id));
+
+
+$(".cityBlock").on("click", function (city) {
+    var city = this.id;
+    console.log(city);
+    fetchImages(city);
 });
