424db4402f0617719111600a32ad2b1ab3598917
abdulrfarag <ar060290@gmail.com>
2022-09-18 18:35:39 -0500

    fixed fetch and error codes

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -1,17 +1,16 @@
-window.addEventListener('load', () => {
+
+let searchbtn= document. querySelector ("#btn")
+searchbtn.addEventListener('click', () => {
     let long;
     let lat;
     let tempDescription = document.querySelector('.temp-description');
     let tempDegree = document.querySelector ('.temp-degree');
     let loctionTimezone = document.querySelector ('.location-timezone');
     let icon = document.querySelector ('.icon');
-    
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+    let chosenCity = document.querySelector ('#city')
 
-            const api = "https://api.openweathermap.org/data/3.0/onecall?lat=41.885&lon=-87.626&units=imperial&exclude=currently,daily&appid=b1bfaa8aef920555a5427b626bedb8c9"
+
+            const api = "https://api.openweathermap.org/data/2.5/weather?q="+ chosenCity.value + "&units=imperial&exclude=currently,daily&appid=b1bfaa8aef920555a5427b626bedb8c9"
             fetch (api)
             .then (response => {
                 return response.json();
@@ -19,20 +18,21 @@ window.addEventListener('load', () => {
              
              .then (data => {
                 console.log (data);
-                const { temp} = data.current;
+                const temp = data.main.temp;
 
 
             //set some elements from the api
-            var iconImg = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
+            var iconImg = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
             tempDegree.textContent= temp;
             icon.setAttribute("src", iconImg)
-            tempDescription.textContent= data.current.weather[0].main
-            loctionTimezone.textContent= data.timezone;
+            tempDescription.textContent= data.weather[0].main
+            loctionTimezone.textContent= data.name;
         });
 
     });
-}
-});
+
+    var city = JSON.parse(localStorage.getItem("currentCity"));
+console.log(city);
 
 
     
