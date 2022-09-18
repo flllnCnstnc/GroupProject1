5b0313d38fdbbb41340227766a90df48839c3b3b
abdulrfarag <ar060290@gmail.com>
2022-09-18 15:51:05 -0500

    fixed "{()}"

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -21,11 +21,18 @@ window.addEventListener('load', () => {
                 console.log (data);
                 const { temp} = data.current;
 
-                
+
             //set some elements from the api
             var iconImg = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
             tempDegree.textContent= temp;
             icon.setAttribute("src", iconImg)
             tempDescription.textContent= data.current.weather[0].main
             loctionTimezone.textContent= data.timezone;
+        });
+
+    });
+}
+});
+
+
     
