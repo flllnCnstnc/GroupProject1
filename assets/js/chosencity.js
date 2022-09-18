45c6555633e3b343747353897c603c9f2d8b9cf1
Ari-chan <arielle.elak@gmail.com>
2022-09-17 18:47:07 -0600

    Ran out of requests for monthly free limit on API. Switching strategy to different API for MVP

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -10,38 +10,47 @@
 /**
  * city variable to be taken from index.html. Defined for testing
  */
-var city = "Denver"
-var restaurantsBlock = $('restaurants-block');
-var requestUrl = '';
+
+  var city = "Boulder"
+  var geoId = 33324;
+  var restaurantsBlock = $('restaurants-block');
+  var requestUrl = 'https://travel-advisor.p.rapidapi.com/restaurants/v2/list?currency=USD&units=mi&lang=en_US';
+
+  $(document).prop('title', "Welcome to " + city);
+  var restaurantsTitle = $('#restaurants-title').text("Places to Eat in " + city);
 
 
-
-$(document).prop('title', "Welcome to " + city);
-var restaurantsTitle = $('#restaurants-title').text("Places to Eat in " + city);
-
-
-
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': '1b5f8f1091mshbc2582aab1d8be2p14f00fjsnce3839947530',
-		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-	},
-	body: '{"geoId":33324,"partySize":2,"reservationTime":"2022-03-07T20:00","sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":12.248278039408776,"longitude":109.1981618106365},"southWestCorner":{"latitude":12.243407232845051,"longitude":109.1921640560031}},"updateToken":""}'
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '1b5f8f1091mshbc2582aab1d8be2p14f00fjsnce3839947530',
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+    },
+    body: '{"geoId":33324,"sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"updateToken":""}'
 };
 
-fetch('https://travel-advisor.p.rapidapi.com/restaurants/v2/list?currency=USD&units=km&lang=en_US', options)
-.then(function (response) {
+var displayRestaurants = function (entries) {
+  console.log("Entries: " + entries);
+}
+
+  fetch(requestUrl).then(function (response) {
     if (response.ok) {
-      console.log(response);
-        response.json().then(function (data) {
-            console.log(data.data.AppPresentation_queryAppListV2);
+      response.json().then(function (data) {
+        console.log(data);
+        displayRestaurants(data.items);
       });
     } else {
-      alert('Error: ' + response.statusText);
+      console.log('Error: ' + response.statusText);
     }
-  })
-  .catch(function (error) {
-    alert('Unable to connect to GitHub');
   });
+
+
+
+  // .filter
+  // "item".cardTitle
+// if(cardTitle)
+// - return
+// Return a new array - var = []
+// Will not change the current array
+//
