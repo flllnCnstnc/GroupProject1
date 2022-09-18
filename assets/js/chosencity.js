e2018c3e321b427f926e1e3ce10fb948c39bf4c0
abdulrfarag <ar060290@gmail.com>
2022-09-18 15:42:44 -0500

    added window.evenlistner

---
+++ assets/js/chosencity.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let tempDescription = document.querySelector('.temp-description');
+    let tempDegree = document.querySelector ('.temp-degree');
+    let loctionTimezone = document.querySelector ('.location-timezone');
+    let icon = document.querySelector ('.icon');
