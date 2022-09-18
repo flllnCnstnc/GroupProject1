35b37b6918798b8ca57636ccc3b47127bb7d985f
Ari-chan <arielle.elak@gmail.com>
2022-09-17 19:09:03 -0600

    Re-structuring project for achievable MVP. Incorporating Pixabay API for home page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,15 @@
 /**
- * 
+ * HomePage JS for Travel Widget
+ * Last Updated by Arielle Sept 17 2022
 **/
+
+
+var API_KEY = '16784074-a09f6dbadf96547b9a326b052';
+var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent('Boulder Colorado');
+
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
