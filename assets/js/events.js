cd936b48694f88ee0ba68efd6cef7be570c57cfc
coflu <cofluellen@gmail.com>
2022-09-19 17:11:18 -0500

    added the info for the href attribute to populate

--- assets/js/events.js
+++ assets/js/events.js
@@ -29,44 +29,65 @@ function showSlides(n) {
 }
 
 //Help pull in the city variable
-var cityStr = localStorage.getItem('currentCity')
+var cityStr = localStorage.getItem('currentCity');
 //var cityObj = JSON.parse(cityStr)
-var cityObj = 'houston'
+var cityObj = '.houston';
 
 
 
-let imgEl = document.querySelector("img");
+var imgEl = document.querySelectorAll("img");
+var siteEl = document.querySelectorAll("a");
 
-function updateImageBoulder() {
+function updateToBoulder() {
   imgEl[0].setAttribute("src", "./assets/images/colorado-musicfestival.jpg");
   imgEl[1].setAttribute("src", "./assets/images/boulder-fall-fest.jpg");
   imgEl[2].setAttribute("src", "./assets/images/bolderboulder.jpg");
   imgEl[3].setAttribute("src", "./assets/images/winter-craft-beer-festival.jpg");
   imgEl[4].setAttribute("src", "./assets/images/b-filmfestival.jpg");
+  siteEl[0].setAttribute("href", "https://coloradomusicfestival.org/")
+  siteEl[1].setAttribute("href", "https://boulderdowntown.com/fall-fest")
+  siteEl[2].setAttribute("href", "https://www.bolderboulder.com/")
+  siteEl[3].setAttribute("href", "https://www.wintercraftbeerfestival.com/")
+  siteEl[4].setAttribute("href", "https://biff1.com/")
 }
 
-function updateImageLA() {
+function updateToLA() {
   imgEl[0].setAttribute("src", "./assets/images/la-film-festival.jpg");
   imgEl[1].setAttribute("src", "./assets/images/la-grammy.jpg");
   imgEl[2].setAttribute("src", "./assets/images/la-emmys.jpg");
   imgEl[3].setAttribute("src", "./assets/images/la-hollywood-bowl.jpg");
   imgEl[4].setAttribute("src", "./assets/images/la-broadway.jpg");
+  siteEl[0].setAttribute("href", "https://www.filmindependent.org/la-film-festival/")
+  siteEl[1].setAttribute("href", "https://www.grammy.com/")
+  siteEl[2].setAttribute("href", "https://www.discoverlosangeles.com/event/2022/09/12/primetime-emmy-awards")
+  siteEl[3].setAttribute("href", "https://www.hollywoodbowl.com/")
+  siteEl[4].setAttribute("href", "https://www.broadwayinhollywood.com/venues/detail/pantagestheatre")
 }
 
-function updateImageChicago() {
+function updateToChicago() {
   imgEl[0].setAttribute("src", "./assets/images/taste-of-chicago.jpg");
   imgEl[1].setAttribute("src", "./assets/images/Lollapalooza-Chicago.jpg");
   imgEl[2].setAttribute("src", "./assets/images/chicago-boat-show.jpg");
   imgEl[3].setAttribute("src", "./assets/images/Air&WaterShow.jpg");
   imgEl[4].setAttribute("src", "./assets/images/chicago-marathon.jpg");
+  siteEl[0].setAttribute("href", "https://www.chicago.gov/city/en/depts/dca/supp")
+  siteEl[1].setAttribute("href", "https://www.lollapalooza.com/")
+  siteEl[2].setAttribute("href", "https://www.chicagoboatshow.com/")
+  siteEl[3].setAttribute("href", "https://www.chicago.gov/city/en/depts/dca/supp")
+  siteEl[4].setAttribute("href", "https://www.chicagomarathon.com/")
 }
 
-function updateImageHouston() {
+function updateToHouston() {
   imgEl[0].setAttribute("src", "./assets/images/houston-rodeo.jpg")
   imgEl[1].setAttribute("src", "./assets/images/miller-theater.jpg")
   imgEl[2].setAttribute("src", "./assets/images/h-art-festival.jpg")
   imgEl[3].setAttribute("src", "./assets/images/comic-cosplay.jpg")
   imgEl[4].setAttribute("src", "./assets/images/art-car.jpg")
+  siteEl[0].setAttribute("href", "https://www.rodeohouston.com/")
+  siteEl[1].setAttribute("href", "https://www.milleroutdoortheatre.com/")
+  siteEl[2].setAttribute("href", "https://www.artcolonyassociation.org/")
+  siteEl[3].setAttribute("href", "https://www.comicpalooza.com/")
+  siteEl[4].setAttribute("href", "https://www.thehoustonartcarparade.com/")
 }
 
 function chooseCity() {
