054e19763a0e1665b93631d03810966ab5e23e74
Ari-chan <arielle.elak@gmail.com>
2022-09-18 21:06:05 -0600

    Slides working for the first of the class it finds - still need method to select all relevant div classes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
 
 var fetchImages = function (searchTerm) {
     var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchTerm) + "&per_page=5";
-    var thisBlock = $('#' + searchTerm + '> ul');
+    var thisBlock = $('#' + searchTerm);
     $.getJSON(URL, function (data) {
         if (parseInt(data.totalHits) > 0)
             $.each(data.hits, function (i, hit) {
@@ -40,20 +40,37 @@ var fetchImages = function (searchTerm) {
     });
 };
 
+// Ongoing counter checks to see if all slides have been pulled
+// Only when they've been pulled can we start the showSlides function for the carousel
 var countFunction = function(a) {
     console.log("Pulling slide: " + a);
     if (a === 20) {
-        showSlides()
+        console.log("All the slides have been pulled")
+        showSlides();
     }
 }
 
 var showSlides = function () {
-    console.log("All the slides have been pulled")
+        $(function () {
+            $ds = $('.cityBlock div');
+            $ds.hide().eq(0).show();
+            setInterval(function(){
+                $ds.filter(':visible').fadeOut(function(){
+                    var $div = $(this).next('div');
+                    if ( $div.length == 0 ) {
+                        $ds.eq(0).fadeIn();
+                    } else {
+                        $div.fadeIn();
+                    }
+                });
+            }, 3000);
+        })
 }
 
 
 
-
+// When any of the cityBlock divs are clicked
+// The city becomes set to the id of the div
 $(".cityBlock").on('click', function (event) {
     var city = this.id;
 
@@ -75,7 +92,7 @@ $(".cityBlock").on('click', function (event) {
         "city": finalCity
     }
 
-
+    // Post object to local storage and load the chosencity.html content
     localStorage.setItem("currentCity", JSON.stringify(currentCity));
     event.preventDefault();
     location.href = "chosencity.html"
