dba7d7bbe7ef45cf3ca523a1d6ae770400518e69
coflu <cofluellen@gmail.com>
2022-09-19 11:16:27 -0500

    wrote the query selector more concisely and ensured that my img elements were written correctly

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,5 +1,5 @@
 // Indicates what slide the carosel starts on
-let slideIndex = 0;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next controls
@@ -33,44 +33,40 @@ var cityStr = localStorage.getItem('currentCity')
 //var cityObj = JSON.parse(cityStr)
 var cityObj = 'houston'
 
-let img1El = document.querySelector("#img1");
-let img2El = document.querySelector("#img2");
-let img3El = document.querySelector("#img3");
-let img4El = document.querySelector("#img4");
-let img5El = document.querySelector("#img5");
 
 
+let imgEl = document.querySelector("img");
 
 function updateImageBoulder() {
-  img1El.setAttribute("src", "./assets/images/colorado-musicfestival.jpg");
-  img2El.setAttribute("src", "./assets/images/boulder-fall-fest.jpg");
-  img3El.setAttribute("src", "./assets/images/bolderboulder.jpg");
-  img4El.setAttribute("src", "./assets/images/winter-craft-beer-festival.jpg");
-  img5El.setAttribute("src", "./assets/images/b-filmfestival.jpg");
+  imgEl[0].setAttribute("src", "./assets/images/colorado-musicfestival.jpg");
+  imgEl[1].setAttribute("src", "./assets/images/boulder-fall-fest.jpg");
+  imgEl[2].setAttribute("src", "./assets/images/bolderboulder.jpg");
+  imgEl[3].setAttribute("src", "./assets/images/winter-craft-beer-festival.jpg");
+  imgEl[4].setAttribute("src", "./assets/images/b-filmfestival.jpg");
 }
 
 function updateImageLA() {
-  img1El.setAttribute("src", "./assets/images/la-film-festival.jpg");
-  img2El.setAttribute("src", "./assets/images/la-grammy.jpg");
-  img3El.setAttribute("src", "./assets/images/la-emmys.jpg");
-  img4El.setAttribute("src", "./assets/images/la-hollywood-bowl.jpg");
-  img5El.setAttribute("src", "./assets/images/la-broadway.jpg");
+  imgEl[0].setAttribute("src", "./assets/images/la-film-festival.jpg");
+  imgEl[1].setAttribute("src", "./assets/images/la-grammy.jpg");
+  imgEl[2].setAttribute("src", "./assets/images/la-emmys.jpg");
+  imgEl[3].setAttribute("src", "./assets/images/la-hollywood-bowl.jpg");
+  imgEl[4].setAttribute("src", "./assets/images/la-broadway.jpg");
 }
 
 function updateImageChicago() {
-  img1El.setAttribute("src", "./assets/images/taste-of-chicago.jpg");
-  img2El.setAttribute("src", "./assets/images/Lollapalooza-Chicago.jpg");
-  img3El.setAttribute("src", "./assets/images/chicago-boat-show.jpg");
-  img4El.setAttribute("src", "./assets/images/Air&WaterShow.jpg");
-  img5El.setAttribute("src", "./assets/images/chicago-marathon.jpg");
+  imgEl[0].setAttribute("src", "./assets/images/taste-of-chicago.jpg");
+  imgEl[1].setAttribute("src", "./assets/images/Lollapalooza-Chicago.jpg");
+  imgEl[2].setAttribute("src", "./assets/images/chicago-boat-show.jpg");
+  imgEl[3].setAttribute("src", "./assets/images/Air&WaterShow.jpg");
+  imgEl[4].setAttribute("src", "./assets/images/chicago-marathon.jpg");
 }
 
 function updateImageHouston() {
-  img1El.setAttribute[("src", "./assets/images/houston-rodeo.jpg")]
-  img2El.setAttribute("src", "./assets/images/miller-theater.jpg")
-  img3El.setAttribute("src", "./assets/images/h-art-festival.jpg")
-  img4El.setAttribute("src", "./assets/images/comic-cosplay.jpg")
-  img5El.setAttribute("src", "./assets/images/art-car.jpg")
+  imgEl[0].setAttribute("src", "./assets/images/houston-rodeo.jpg")
+  imgEl[1].setAttribute("src", "./assets/images/miller-theater.jpg")
+  imgEl[2].setAttribute("src", "./assets/images/h-art-festival.jpg")
+  imgEl[3].setAttribute("src", "./assets/images/comic-cosplay.jpg")
+  imgEl[4].setAttribute("src", "./assets/images/art-car.jpg")
 }
 
 function chooseCity() {
