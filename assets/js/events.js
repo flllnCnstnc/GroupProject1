d6a03faf89e7448e79d06d1baf4444dbcd4336a4
coflu <cofluellen@gmail.com>
2022-09-19 05:13:10 -0500

    file paths in the updateImage folders applied to the correct ciyt

--- assets/js/events.js
+++ assets/js/events.js
@@ -28,55 +28,62 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-//Help pull in the variable
-var city = JSON.parse(localStorage.getItem("currentCity"));
-console.log(city);
+//Help pull in the city variable
+var cityStr = localStorage.getItem('currentCity')
+//var cityObj = JSON.parse(cityStr)
+var cityObj = 'houston'
 
-let img1El = document.querySelector("#img1")
-let img2El = document.querySelector("#img2")
-let img3El = document.querySelector("#img3")
-let img4El = document.querySelector("#img4")
-let img5El = document.querySelector("#img5")
+let img1El = document.querySelector("#img1");
+let img2El = document.querySelector("#img2");
+let img3El = document.querySelector("#img3");
+let img4El = document.querySelector("#img4");
+let img5El = document.querySelector("#img5");
 
-var chicagoBtn = document.querySelector("#chicago-illinois")
-var boulderBtn = document.querySelector("#boulder-colorado")
-var losAngelesBtn = document.querySelector("#los-angeles-calafornia")
 
-coloradoBtn.addEventListener('click', function(){
-  let thisCIty = inputEl.value.trim()
-  updateImage(thisCity)
-})
 
-function updateImage(colorado){
-  img1El.setAttribute("src", "./images/colorado-musicfestival.jpg")
-  img2El.setAttribute("src", "./images/boulder-fall-fest.jpg")
-  img3El.setAttribute("src", "./images/bolderboulder.jpg")
-  img4El.setAttribute("src", "./images/winter-craft-beer-festival.jpg")
-  img5El.setAttribute("src", "./images/b-filmfestival.jpg")
+function updateImageBoulder() {
+  img1El.setAttribute("src", "./assets/images/colorado-musicfestival.jpg");
+  img2El.setAttribute("src", "./assets/images/boulder-fall-fest.jpg");
+  img3El.setAttribute("src", "./assets/images/bolderboulder.jpg");
+  img4El.setAttribute("src", "./assets/images/winter-craft-beer-festival.jpg");
+  img5El.setAttribute("src", "./assets/images/b-filmfestival.jpg");
 }
 
-losAngelesBtn.addEventListener('click', function(){
-  let thisCIty = inputEl.value.trim()
-  updateImage(thisCity)
-})
-
-function updateImage(losAngeles){
-  img1El.setAttribute("src", "./images/colorado-musicfestival.jpg")
-  img2El.setAttribute("src", "./images/boulder-fall-fest.jpg")
-  img3El.setAttribute("src", "./images/bolderboulder.jpg")
-  img4El.setAttribute("src", "./images/winter-craft-beer-festival.jpg")
-  img5El.setAttribute("src", "./images/b-filmfestival.jpg")
+function updateImageLA() {
+  img1El.setAttribute("src", "./assets/images/la-film-festival.jpg");
+  img2El.setAttribute("src", "./assets/images/la-grammy.jpg");
+  img3El.setAttribute("src", "./assets/images/la-emmys.jpg");
+  img4El.setAttribute("src", "./assets/images/la-hollywood-bowl.jpg");
+  img5El.setAttribute("src", "./assets/images/la-broadway.jpg");
 }
 
-boulderBtn.addEventListener('click', function(){
-  let thisCIty = inputEl.value.trim()
-  updateImage(thisCity)
-})
+function updateImageChicago() {
+  img1El.setAttribute("src", "./assets/images/taste-of-chicago.jpg");
+  img2El.setAttribute("src", "./assets/images/Lollapalooza-Chicago.jpg");
+  img3El.setAttribute("src", "./assets/images/chicago-boat-show.jpg");
+  img4El.setAttribute("src", "./assets/images/Air&WaterShow.jpg");
+  img5El.setAttribute("src", "./assets/images/chicago-marathon.jpg");
+}
 
-function updateImage(boulder){
-  img1El.setAttribute("src", "./images/colorado-musicfestival.jpg")
-  img2El.setAttribute("src", "./images/boulder-fall-fest.jpg")
-  img3El.setAttribute("src", "./images/bolderboulder.jpg")
-  img4El.setAttribute("src", "./images/winter-craft-beer-festival.jpg")
-  img5El.setAttribute("src", "./images/b-filmfestival.jpg")
+function updateImageHouston() {
+  img1El.setAttribute[("src", "./assets/images/houston-rodeo.jpg")]
+  img2El.setAttribute("src", "./assets/images/miller-theater.jpg")
+  img3El.setAttribute("src", "./assets/images/h-art-festival.jpg")
+  img4El.setAttribute("src", "./assets/images/comic-cosplay.jpg")
+  img5El.setAttribute("src", "./assets/images/art-car.jpg")
+}
+
+function chooseCity() {
+  if(cityObj === "houston") {
+    updateImageHouston();
+  }
+  if(cityObj === "los-angeles") {
+    updateImageLosAngeles();
+  }
+  if(cityObj === "boulder") {
+    updateImageBoulder();
+  }
+  if(cityObj === "chicago") {
+    updateImageChicago();
+  }
 }
