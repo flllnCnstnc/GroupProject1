9209df99d62eec2a828f08f357861c36f8cd8f53
abdulrfarag <ar060290@gmail.com>
2022-09-22 02:42:31 -0500

    fixed my api code

--- assets/js/chosencity.js
+++ assets/js/chosencity.js
@@ -169,17 +169,17 @@ function updateHouston() {
  * START Abdul
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
- let searchbtn= document. querySelector ("#btn")
- searchbtn.addEventListener('click', () => {
+
+// calling for the value from the local storage
+localStorage.getItem(city)
+
 
      let tempDescription = document.querySelector('.temp-description');
-     let tempDegree = document.querySelector ('.temp-degree');
-     let loctionTimezone = document.querySelector ('.location-timezone');
+   
      let icon = document.querySelector ('.icon');
-     let chosenCity = document.querySelector ('#city')
-
-
-             const api = "https://api.openweathermap.org/data/2.5/weather?q="+ chosenCity.value + "&units=imperial&exclude=currently,daily&appid=b1bfaa8aef920555a5427b626bedb8c9"
+  
+            
+     const api = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&exclude=currently,daily&appid=b1bfaa8aef920555a5427b626bedb8c9"
              fetch (api)
              .then (response => {
                  return response.json();
@@ -187,18 +187,23 @@ function updateHouston() {
 
               .then (data => {
                  console.log (data);
-                 const temp = data.main.temp;
-
-
-             //set some elements from the api
+            
+                 //set some elements from the api
              var iconImg = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-             tempDegree.textContent= temp;
+             document.querySelector('.temp-degree').innerText = data.main.temp + "° F";
              icon.setAttribute("src", iconImg)
              tempDescription.textContent= data.weather[0].main
-             loctionTimezone.textContent= data.name;
+            
+             document.querySelector('.city-weather').innerText= data.name;
+             document.querySelector(".hi-temp").innerText =  data.main.temp_max +'°' ;
+             document.querySelector(".low-temp").innerText =  data.main.temp_min +'°' ;
+
+
+            
+
          });
 
-     });
+     
 
 /**
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
